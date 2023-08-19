6a812c18819ad0be4413d8c27164f07fdb0ea4f2
erAnusriM <97402672+erAnusriM@users.noreply.github.com>
2023-08-19 13:57:27 +0530

    minor change in stored procedure

--- Anusri_Mukhopadhyay_Graded_Lab4_DBMS.sql
+++ Anusri_Mukhopadhyay_Graded_Lab4_DBMS.sql
@@ -182,21 +182,20 @@ select CUS_NAME, CUS_GENDER from customer where CUS_NAME LIKE "%A" OR CUS_NAME L
 -- Type_of_Service. For Type_of_Service, If rating =5, print “Excellent Service”,If rating >4 print “Good Service”, If rating >2 print “Average
 -- Service” else print “Poor Service”. Note that there should be one rating per supplier.
 
-CREATE DEFINER=`root`@`localhost` PROCEDURE `rating_proc`()
+DELIMITER &&
+create procedure sp_getSupplierRating()
 BEGIN
-select report.supp_id,report.supp_name,report.Average,
+select A.supp_id, A.supp_name, 
 CASE
-	WHEN report.Average =5 THEN 'Excellent Service'
-    	WHEN report.Average >4 THEN 'Good Service'
-    	WHEN report.Average >2 THEN 'Average Service'
-    	ELSE 'Poor Service'
-END AS Type_of_Service from 
-(select final.supp_id, supplier.supp_name, final.Average from
-(select test2.supp_id, sum(test2.rat_ratstars)/count(test2.rat_ratstars) as Average from
-(select supplier_pricing.supp_id, test.ORD_ID, test.RAT_RATSTARS from supplier_pricing inner join
-(select `order`.pricing_id, rating.ORD_ID, rating.RAT_RATSTARS from `order` inner join rating on rating.`ord_id` = `order`.ord_id ) as test
-on test.pricing_id = supplier_pricing.pricing_id) 
-as test2 group by test2.supp_id) 
-as final inner join supplier where final.supp_id = supplier.supp_id) as report;
-END
+    WHEN avg(A.rating) = 5 THEN 'Excellent'
+    WHEN avg(A.rating) > 4 THEN 'Good Service'
+	WHEN avg(A.rating) > 2 THEN 'Average Service'
+    ELSE 'Poor Service'
+END AS Rating
+from (select r.rat_ratstars as rating, o.*, s.supp_id, s.supp_name from rating r, `order` o, supplier_pricing p, supplier s where r.ord_id = o.ord_id and o.pricing_id = p.pricing_id and p.supp_id = s.supp_id order by p.supp_id) as A group by A.supp_id order by s.supp_id LIMIT 0,100;
+
+END 
+&& DELIMITER ;
+
+Call sp_getSupplierRating(); 
 
